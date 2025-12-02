75a9aa1082fa8907ca7cce81aeea25bfc8595118
DravenPorschE <dravenporsch@gmai.com>
2025-12-02 18:12:41 +0800

    test the gif display

--- displayGif.py
+++ displayGif.py
@@ -2,6 +2,12 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 root = Tk()
+# Set fixed screen size
+screen_width = 480
+screen_height = 320
+root.geometry(f"{screen_width}x{screen_height}")
+root.resizable(False, False)
+root.configure(bg="white")
 
 # Load the GIF using PIL
 image = Image.open("animation.gif")
